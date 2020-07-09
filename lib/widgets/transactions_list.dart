24e149356b76021369bf509cf4c79490c6c34e00
Adrian Badarau <adi.badarau@yahoo.com>
2020-07-09 20:26:20 +0200

    added more scrolling

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -9,49 +9,54 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: _transactionService.getTransactions().map((Transaction tx) {
-      return Card(
-        child: Row(
-          children: <Widget>[
-            Container(
-              child: Text(
-                "${tx.amount.toString()} $kMainCurrency",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              ),
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 15,
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: Colors.purple),
-              ),
-              padding: EdgeInsets.all(10),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 450,
+      child: SingleChildScrollView(
+        child: Column(
+            children: _transactionService.getTransactions().map((Transaction tx) {
+          return Card(
+            child: Row(
               children: <Widget>[
-                Text(
-                  tx.title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+                Container(
+                  child: Text(
+                    "${tx.amount.toString()} $kMainCurrency",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
                   ),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.purple),
+                  ),
+                  padding: EdgeInsets.all(10),
                 ),
-                Text(
-                  DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY)
-                      .format(tx.createdAt),
-                  style: TextStyle(color: Colors.grey),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY)
+                          .format(tx.createdAt),
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
-        ),
-      );
-    }).toList());
+            ),
+          );
+        }).toList()),
+      ),
+    );
   }
 
   TransactionsList({TransactionService transactionService})
