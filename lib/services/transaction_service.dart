ea571c42e2090b4d8f493e4dd4bf47c2ec20c2a5
Adrian Badarau <adi.badarau@yahoo.com>
2020-07-19 12:49:11 +0300

    added empty list design

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,14 +1,7 @@
 import 'package:expense_tracker/domain/transaction.dart';
 
 class TransactionService {
-  final List<Transaction> _transactions = [
-    Transaction(
-        id: 'id-1', title: 'Berries', amount: 99.5, createdAt: DateTime.now()),
-    Transaction(
-        id: 'id-2', title: 'Apples', amount: 22.0, createdAt: DateTime.now()),
-    Transaction(
-        id: 'id-3', title: 'Chicken', amount: 53.1, createdAt: DateTime.now()),
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> getTransactions() {
     return _transactions;
--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -1,5 +1,4 @@
 import 'package:expense_tracker/config/constants.dart';
-import 'package:expense_tracker/domain/transaction.dart';
 import 'package:expense_tracker/services/transaction_service.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -11,49 +10,62 @@ class TransactionsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 450,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          var tx = _transactionService.getTransactions()[index];
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    "${tx.amount.toStringAsFixed(2)} $kMainCurrency",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor),
+      child: _transactionService.getTransactions().isEmpty
+          ? Column(children: <Widget>[
+              Text(
+                'There are no items yet',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(height: 10),
+              Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ))
+            ])
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                var tx = _transactionService.getTransactions()[index];
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          "${tx.amount.toStringAsFixed(2)} $kMainCurrency",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 2, color: Theme.of(context).primaryColor),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(tx.title,
+                              style: Theme.of(context).textTheme.headline6),
+                          Text(
+                            DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY)
+                                .format(tx.createdAt),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
                   ),
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Theme.of(context).primaryColor),
-                  ),
-                  padding: EdgeInsets.all(10),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: Theme.of(context).textTheme.headline6
-                    ),
-                    Text(
-                      DateFormat(DateFormat.YEAR_ABBR_MONTH_DAY)
-                          .format(tx.createdAt),
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                )
-              ],
+                );
+              },
+              itemCount: _transactionService.getTransactions().length,
             ),
-          );
-        },
-        itemCount: _transactionService.getTransactions().length,
-      ),
     );
   }
 
