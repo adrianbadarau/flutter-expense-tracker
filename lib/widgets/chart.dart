16887cdf59fc4cc9d55eb0f5cfa34711ab1c4493
Adrian Badarau <adi.badarau@yahoo.com>
2020-07-20 09:46:47 +0300

    added the chart drawing logic

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,14 @@
 import 'package:expense_tracker/domain/transaction.dart';
+import 'package:expense_tracker/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ChartColumn {
   final String day;
   final double amount;
+  final double procOfTotal;
 
-  ChartColumn({this.day, this.amount});
+  ChartColumn({this.procOfTotal, this.day, this.amount});
 }
 
 class Chart extends StatelessWidget {
@@ -15,13 +17,28 @@ class Chart extends StatelessWidget {
   Chart({this.recentTransactions});
 
   List<ChartColumn> get groupedTransactionValues {
+    final total = recentTransactions.fold(
+        0, (previousValue, element) => previousValue + element.amount);
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
       double sum = recentTransactions
           .where((element) => element.createdAt.difference(weekDay).inDays == 0)
           .fold(0, (previousValue, element) => previousValue + element.amount);
-      return ChartColumn(day: DateFormat.E().format(weekDay), amount: sum);
+      return ChartColumn(
+          day: DateFormat.E().format(weekDay),
+          amount: sum,
+          procOfTotal: sum / total);
     });
+  }
+
+  List<Widget> buildChartItems() {
+    return this.groupedTransactionValues.map((column) {
+      return ChartBar(
+        label: column.day,
+        spendingAmount: column.amount,
+        spendingProcOfTotal: column.procOfTotal,
+      );
+    }).toList();
   }
 
   @override
@@ -31,9 +48,7 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[
-
-        ],
+        children: buildChartItems(),
       ),
     );
   }
